e0e5ed5d4d072e120ad91818bd4e163635da9744
Oran Mehavi <oranhero@gmail.com>
2022-10-05 00:24:59 +0300

    Show books in the grid

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ const authorField = dialog.querySelector("#author");
 const nameField = dialog.querySelector("#book-name");
 const pagesField = dialog.querySelector("#pages");
 const checkBox = dialog.querySelector("#isRead");
+const bookContainer = document.querySelector(".books-container");
 
 function Book(author, bookName, pages, isRead) {
     this.author = author;
@@ -34,4 +35,41 @@ dialog.addEventListener('close', () => {
 
 confirmButton.addEventListener('click', () => {
     addBookToLibrary(authorField.value, nameField.value, pages.value, checkBox.checked);
+    showBooks();
 });
+
+
+function showBooks() {
+    if (myLibrary.length > 0) {
+        bookContainer.innerHTML = '';
+        for(b of myLibrary) {
+            let bookCard = document.createElement('div');
+            let title = document.createElement('h1');
+            let bookAuthor = document.createElement('h2');
+            let bookPages = document.createElement('h3');
+            let readStatus = document.createElement('h3');
+            let readToggle = document.createElement('input');
+
+
+            title.innerText = "Book name: " + b.bookName;
+            bookAuthor.innerText = "Written by: " + b.author;
+            bookPages.innerText = `${b.pages} pages`;
+            if (b.isRead) {
+                readStatus.innerText = "The book is read";
+            }
+            else { 
+                readStatus.innerText = "The book is not read";
+            }
+            readToggle.setAttribute('type', 'checkbox');
+            readToggle.checked = b.isRead;
+            bookCard.appendChild(title);
+            bookCard.appendChild(bookAuthor);
+            bookCard.appendChild(bookPages);
+            bookCard.appendChild(readStatus);
+            bookCard.appendChild(readToggle);
+            bookCard.classList.add('book-card');
+            bookContainer.appendChild(bookCard);
+        }
+        
+    }
+}
